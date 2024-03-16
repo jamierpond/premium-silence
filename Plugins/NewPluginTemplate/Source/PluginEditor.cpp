0665f287c0136eac81f9d542b92cf8a3eb45a4f3
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-03-15 21:41:14 -0700

    make bg work

--- Plugins/NewPluginTemplate/Source/PluginEditor.cpp
+++ Plugins/NewPluginTemplate/Source/PluginEditor.cpp
@@ -5,17 +5,29 @@ PremiumSilenceAudioProcessorEditor::PremiumSilenceAudioProcessorEditor(
     PremiumSilenceAudioProcessor& p)
     : AudioProcessorEditor(&p)
 {
-    addAndMakeVisible(editor);
-    auto bgSize = background.getBounds();
-    setSize(bgSize.getWidth(), bgSize.getHeight());
+    // addAndMakeVisible(editor);
+    auto bgSize = background.getBounds().toFloat();
+    auto bgWidth = bgSize.getWidth();
+    auto bgHeight = bgSize.getHeight();
+
+    auto ratio = bgWidth / bgHeight;
+
+    auto initialWidth = 800.0f;
+    auto initialHeight = initialWidth / ratio;
+
+    setResizeLimits(400, 400.0/ratio, 1200, 1200.0/ratio);
+    getConstrainer()->setFixedAspectRatio(ratio);
+    setSize(800.0,800.0/ratio);
 }
 
 void PremiumSilenceAudioProcessorEditor::paint(juce::Graphics& g)
 {
-  g.drawImageAt(background, 0, 0);
+    // draw the image to fit the available space
+    auto bound = getLocalBounds();
+    g.drawImage(background, bound.toFloat());
 }
 
 void PremiumSilenceAudioProcessorEditor::resized()
 {
-    editor.setBounds(getLocalBounds());
+    // editor.setBounds(getLocalBounds());
 }
--- Plugins/NewPluginTemplate/Source/PluginEditor.h
+++ Plugins/NewPluginTemplate/Source/PluginEditor.h
@@ -12,7 +12,7 @@ private:
     void paint(juce::Graphics&) override;
     void resized() override;
 
-    juce::GenericAudioProcessorEditor editor {processor};
+    // juce::GenericAudioProcessorEditor editor {processor};
 
     juce::Image background = juce::ImageCache::getFromMemory(BinaryData::bg_text_png, BinaryData::bg_text_pngSize);
 };
